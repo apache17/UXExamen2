ab7ba56d36ec1634d7c5aed560733b90bec70213
Boris Garcia <brgp@live.com>
2018-04-02 18:01:38 -0600

    Controladores y Modelos

--- backend/Examen2/db/schema.rb
+++ backend/Examen2/db/schema.rb
@@ -10,24 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180402233824) do
+ActiveRecord::Schema.define(version: 20180402235649) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tweets", force: :cascade do |t|
     t.text "body"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_tweets_on_user_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "user"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "tweets", "users"
 end
